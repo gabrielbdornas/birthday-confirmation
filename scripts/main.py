acfacb6f6987b057881c6a760a11fef273f79e3d
Gabriel Dornas <gabrielbdornas@gmail.com>
2025-01-04 17:36:55 -0300

    Remove hamburguer menu

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,26 @@
 import streamlit as st
 from supabase_conection import init_supabase, save_to_supabase
 
+def remove_streamlit_hamburguer():
+    hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
 def main():
+
+    st.set_page_config(
+        page_title="Gabriel 40 Anos - Confirme presença nesta festança",
+        page_icon=":beers:",
+        menu_items={}  # Disable the hamburger menu
+    )
+
+    remove_streamlit_hamburguer()
+
     st.title(":tada: Gabriel 40 Anos! :beers:")
 
     st.write(f"**Confirme presença nesta festança**")
